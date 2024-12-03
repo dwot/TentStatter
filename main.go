9d7c7f9fc882d73f2ff561d07cc62900fb307cf5
dwot <68145+dwot@users.noreply.github.com>
2024-12-02 22:38:03 -0500

    Update for AC Infinity AI controller

--- main.go
+++ main.go
@@ -31,15 +31,24 @@ type DeviceData struct {
 }
 
 type DeviceInfo struct {
-	TemperatureF int    `json:"temperatureF"`
-	Humidity     int    `json:"humidity"`
-	Ports        []Port `json:"ports"`
+	TemperatureF int      `json:"temperatureF"`
+	Humidity     int      `json:"humidity"`
+	Ports        []Port   `json:"ports"`
+	Sensors      []Sensor `json:"sensors"`
 }
 
 type Port struct {
 	PortName string `json:"portName"`
 	Speak    int    `json:"speak"`
 	Port     int    `json:"port"`
+	CurMode  int    `json:"curMode"`
+	Online   int    `json:"online"`
+}
+
+type Sensor struct {
+	SensorType int `json:"sensorType"`
+	AccessPort int `json:"accessPort"`
+	SensorData int `json:"sensorData"`
 }
 
 func main() {
@@ -67,7 +76,8 @@ func main() {
 		case "token":
 			config.Token = value
 		case "start_date":
-			config.StartDate, err = time.Parse("2006-01-02", value)
+			loc, _ := time.LoadLocation(config.Tz)
+			config.StartDate, err = time.ParseInLocation("2006-01-02 15:04:05", value, loc)
 			if err != nil {
 				log.Fatalf("Failed to parse start_date: %s", err)
 			}
@@ -89,10 +99,11 @@ func main() {
 
 		startDate := config.StartDate
 		currentDate := time.Now().In(location)
-
+		log.Printf("Current date: %v Start date: %v", currentDate, startDate)
 		duration := currentDate.Sub(startDate)
-		days := int(duration.Hours() / 24)
-
+		log.Printf("Duration: %v", duration)
+		days := int(duration.Hours()/24) + 1
+		log.Printf("Days: %d", days)
 		formattedOutput := fmt.Sprintf("Day %d", days)
 		err = ioutil.WriteFile("days.txt", []byte(formattedOutput), 0644)
 		if err != nil {
@@ -139,32 +150,73 @@ func main() {
 		}
 
 		if len(jsonResponse.Data) > 0 {
-			temperatureF := float64(jsonResponse.Data[0].DeviceInfo.TemperatureF) / 100.0
-			humidity := float64(jsonResponse.Data[0].DeviceInfo.Humidity) / 100.0
-
-			formattedOutput := fmt.Sprintf("Temp: %.1f°", temperatureF)
-			err = ioutil.WriteFile("temperatureF.txt", []byte(formattedOutput), 0644)
-			if err != nil {
-				log.Printf("Error writing to file: %v", err)
-			}
-			formattedOutput = fmt.Sprintf("Humidity: %.1f%%", humidity)
-			err = ioutil.WriteFile("humidity.txt", []byte(formattedOutput), 0644)
-			if err != nil {
-				log.Printf("Error writing to file: %v", err)
-			}
-			log.Printf("Temperature: %.1fF, Humidity: %.1f%%", temperatureF, humidity)
-
 			for _, deviceData := range jsonResponse.Data {
+				for _, sensor := range deviceData.DeviceInfo.Sensors {
+					if sensor.SensorType == 0 { //Inside Temp
+						value := float64(sensor.SensorData) / 100.0
+						formattedOutput := fmt.Sprintf("Temp: %.1fÂ°", value)
+						err = ioutil.WriteFile("inside_temp.txt", []byte(formattedOutput), 0644)
+						if err != nil {
+							log.Printf("Error writing to file: %v", err)
+						}
+					} else if sensor.SensorType == 2 { //Inside Humidity
+						value := float64(sensor.SensorData) / 100.0
+						formattedOutput = fmt.Sprintf("Humidity: %.1f%%", value)
+						err = ioutil.WriteFile("inside_humidity.txt", []byte(formattedOutput), 0644)
+						if err != nil {
+							log.Printf("Error writing to file: %v", err)
+						}
+					} else if sensor.SensorType == 3 { //Inside VPD
+						value := float64(sensor.SensorData) / 100.0
+						formattedOutput = fmt.Sprintf("VPD: %.1f", value)
+						err = ioutil.WriteFile("vpd.txt", []byte(formattedOutput), 0644)
+						if err != nil {
+							log.Printf("Error writing to file: %v", err)
+						}
+					} else if sensor.SensorType == 4 { //Outside Temp
+						value := float64(sensor.SensorData) / 100.0
+						formattedOutput := fmt.Sprintf("Temp: %.1fÂ°", value)
+						err = ioutil.WriteFile("outside_temp.txt", []byte(formattedOutput), 0644)
+						if err != nil {
+							log.Printf("Error writing to file: %v", err)
+						}
+					} else if sensor.SensorType == 6 { //Outside Humidity
+						value := float64(sensor.SensorData) / 100.0
+						formattedOutput = fmt.Sprintf("Humidity: %.1f%%", value)
+						err = ioutil.WriteFile("outside_humidity.txt", []byte(formattedOutput), 0644)
+						if err != nil {
+							log.Printf("Error writing to file: %v", err)
+						}
+						/*} else if sensor.SensorType == 7 { //Outside VPD
+						value := float64(sensor.SensorData) / 100.0
+						formattedOutput = fmt.Sprintf("VPD: %.1f", value)
+						err = ioutil.WriteFile("outside_vpd.txt", []byte(formattedOutput), 0644)
+						if err != nil {
+							log.Printf("Error writing to file: %v", err)
+						}*/
+					}
+				}
+
 				for _, port := range deviceData.DeviceInfo.Ports {
-					formattedOutput = fmt.Sprintf("%s: %d%%", port.PortName, port.Speak*10)
-					err = ioutil.WriteFile("port_"+strconv.Itoa(port.Port)+".txt", []byte(formattedOutput), 0644)
-					if err != nil {
-						log.Printf("Error writing to file: %v", err)
+					if port.Online == 1 {
+						formattedOutput = fmt.Sprintf("%s: %d%%", port.PortName, port.CurMode*10)
+						err = ioutil.WriteFile("port_"+strconv.Itoa(port.Port)+".txt", []byte(formattedOutput), 0644)
+						if err != nil {
+							log.Printf("Error writing to file: %v", err)
+						}
+					} else {
+						formattedOutput = "Offline"
+						err = ioutil.WriteFile("port_"+strconv.Itoa(port.Port)+".txt", []byte(formattedOutput), 0644)
+						if err != nil {
+							log.Printf("Error writing to file: %v", err)
+						}
 					}
 				}
 			}
-		}
 
-		time.Sleep(config.UpdateInterval * time.Second)
+			log.Printf("Sleeping for 15 seconds", config.UpdateInterval)
+			time.Sleep(15 * time.Second)
+			log.Printf("Waking up")
+		}
 	}
 }
